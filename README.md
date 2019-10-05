bbb5f19b7dc8ecaa937d109de7894f2bfdb75abe
yasminamayot <yasminamayot@gmail.com>
2019-10-05 14:33:22 -0400

    i forgot a comma

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 MMED 3039 HW1 Heroku Test
 Let's do this!!!
 
+ACCORDING TO GOOGLE:
 for node modules:
 run 'npm install'
 open on the live developer server
 for the node_modules to work!
+
+npm install
+npm start
+it should be good to go on server 3000
--- data.js
+++ data.js
@@ -4,24 +4,25 @@ const data = {
 
     filters: {
         name: "Filters",
-        desc: "C1 Filter",
+        desc: "The Iconic C1 Filter",
         info: "The C1 Filter is a saturated popularized photo filter that highlights blue water and green palm trees! Take a picture using this filter and watch your social media blowup!",
         pic: "pic12.jpg"
+        pic2: "dot-grid.png"
     },
 
     photos: {
-        name: "something",
-        desc: "",
-        info: "",
-        pic: ""
+        name: "Instagram 101",
+        desc: "Knowing How To Take Your Photos",
+        info: "Posing for photos in a nonchalant manner is what's in these days. Make sure to pucker your lips and wear a pastel outfit because girl (or guy!), you're killing it!",
+        pic: "pic9.jpg"
 
     },
 
     aspirations: {
-        name: "",
-        desc: "",
-        info: "",
-        pic: ""
+        name: "Living Your VSCO Dreams!",
+        desc: "A How-To on Your Greatest Aspirations",
+        info: "Being a VSCO Girl isn't just a choice of shoes, clothing, makeup, and hair, it's all about that LIFESTYLE! Make sure to book expensive flights, annoy your friends by asking them to snap photos of you every 10 minutes, and drink from a hydro flask!",
+        pic: "pic11.jpg"
     },
 };
 
